6ac1f44c3ef6819b43b440ca67295a2cf8741614
Kalax <erdelyi.peter04@gmail.com>
2025-05-08 10:37:29 +0200

    Book properties created.

--- NI_Project.Shared/Book.cs
+++ NI_Project.Shared/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace NI_Project.Shared
 {
     internal class Book
     {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        [Range(0, 2025)]
+        public int YearOfPublish { get; set; }
     }
 }
