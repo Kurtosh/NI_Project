f8ea6ec8c4743bcdd787f04147bd9afe5322a61d
Kalax <erdelyi.peter04@gmail.com>
2025-05-08 10:48:23 +0200

    Loan.cs and properties created.

---
+++ NI_Project.Shared/Loan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NI_Project.Shared
+{
+    internal class Loan
+    {
+        [Required]
+        public string ReaderId { get; set; }
+        [Required]
+        public string BookId { get; set; }
+        [Required]
+        public DateTime LoanDate { get; set; }
+        [Required]
+        public DateTime ReturnDeadline { get; set; }
+    }
+}
