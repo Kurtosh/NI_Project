1eda27afaf159e080c4825e3be8c7862965d5fa6
Kalax <erdelyi.peter04@gmail.com>
2025-05-08 11:21:52 +0200

    IBookService.cs implemented in BookService.cs.

---
+++ NI_Project.Api/Services/BookService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NI_Project.Api.Interfaces;
+using NI_Project.Shared;
+
+namespace NI_Project.Api.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly List<Book> _books;
+        public void Add(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public void Delete(string id)
+        {
+            _books.RemoveAll(x => x.Id == id);
+        }
+
+        public Book Get(string id)
+        {
+            return _books.Find(x => x.Id == id);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _books;
+        }
+
+        public void Update(Book book)
+        {
+            var oldBook = Get(book.Id);
+
+            oldBook.Title = book.Title;
+            oldBook.Author = book.Author;
+            oldBook.Publisher = book.Publisher;
+            oldBook.YearOfPublish = book.YearOfPublish;
+        }
+    }
+}
