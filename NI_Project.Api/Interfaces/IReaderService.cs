b22e65852b2e9c45935fcfc317dd48b5f68bdebc
Kalax <erdelyi.peter04@gmail.com>
2025-05-08 11:08:31 +0200

    IReaderService.cs created.

---
+++ NI_Project.Api/Interfaces/IReaderService.cs
@@ -0,0 +1,17 @@
+﻿using NI_Project.Shared;
+
+namespace NI_Project.Api.Interfaces
+{
+    public interface IReaderService
+    {
+        void Add(Reader reader);
+
+        void Delete(string id);
+
+        List<Reader> GetAll();
+
+        Reader Get(string id);
+
+        void Update(Reader reader);
+    }
+}
