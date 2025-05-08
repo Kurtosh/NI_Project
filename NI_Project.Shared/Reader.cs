14b8bed677cf218cb0a09e7bb03f3f410238e66f
Kalax <erdelyi.peter04@gmail.com>
2025-05-08 10:43:23 +0200

    Reader.cs and properties created.

---
+++ NI_Project.Shared/Reader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NI_Project.Shared
+{
+    internal class Reader
+    {
+        [Required]
+        public string Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        [Range(typeof(DateOnly), "1900-01-01", "2025-01-01")]
+        public DateOnly? BirthDate { get; set; }
+    }
+}
