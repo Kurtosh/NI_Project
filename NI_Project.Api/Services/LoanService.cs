0469defccc18fd798b0de84be641cb390df2ce01
Kalax <erdelyi.peter04@gmail.com>
2025-05-10 11:32:45 +0200

    LoanService.cs created and implemented.

---
+++ NI_Project.Api/Services/LoanService.cs
@@ -0,0 +1,24 @@
+﻿using NI_Project.Api.Interfaces;
+using NI_Project.Shared;
+
+namespace NI_Project.Api.Services
+{
+    public class LoanService : ILoanService
+    {
+        private readonly List<Loan> _loans;
+        public void Add(Loan loan)
+        {
+            _loans.Add(loan);
+        }
+
+        public Loan Get(string readerId, string bookId)
+        {
+            return _loans.Find(x => x.ReaderId == readerId && x.BookId == bookId);
+        }
+
+        public List<Loan> GetAll()
+        {
+            return _loans;
+        }
+    }
+}
