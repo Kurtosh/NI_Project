bcf7e434a08a46a22f95acb472de7aa32ef4a139
Kalax <erdelyi.peter04@gmail.com>
2025-05-10 11:24:35 +0200

    ILoanService.cs created and implemented.

---
+++ NI_Project.Api/Interfaces/ILoanService.cs
@@ -0,0 +1,13 @@
+﻿using NI_Project.Shared;
+
+namespace NI_Project.Api.Interfaces
+{
+    public interface ILoanService
+    {
+        void Add(Loan loan);
+
+        List<Loan> GetAll();
+
+        Loan Get(string readerId, string bookId);
+    }
+}
