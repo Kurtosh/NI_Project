84ce9ce78e8199c463340e114938bd630cee8775
Kalax <erdelyi.peter04@gmail.com>
2025-05-11 12:25:36 +0200

    ReadersService.cs created and implemented.

---
+++ NI_Project.UI/Services/ReadersService.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Http.Json;
+using NI_Project.Shared;
+
+namespace NI_Project.UI.Services
+{
+    public class ReadersService : IReadersService
+    {
+        private readonly HttpClient _httpClient;
+
+        public ReadersService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task AddReaderAsync(Reader reader)
+        {
+            await _httpClient.PostAsJsonAsync("readers", reader);
+        }
+
+        public async Task DeleteReaderAsync(string id)
+        {
+            await _httpClient.DeleteAsync($"readers/{id}");
+        }
+
+        public async Task<Reader> GetReaderAsync(string id)
+        {
+            return await _httpClient.GetFromJsonAsync<Reader>($"readers/{id}");
+        }
+
+        public async Task<List<Reader>> GetReadersAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<Reader>>("readers");
+        }
+
+        public async Task UpdateReaderAsync(string id, Reader reader)
+        {
+            await _httpClient.PutAsJsonAsync($"readers/{id}", reader);
+        }
+    }
+}
