4b78d46a7004c3a770d281724775d96d77876754
Kalax <erdelyi.peter04@gmail.com>
2025-05-11 10:07:11 +0200

    Changes to LoanService.cs, LoanServiceUnitTests.cs implemented.

---
+++ NI_Project.Api.Tests/LoanServiceUnitTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NI_Project.Api.Services;
+using NI_Project.Shared;
+
+namespace NI_Project.Api.Tests
+{
+    public class LoanServiceUnitTests
+    {
+        [Fact]
+        public void InvalidLoan_ShouldThrowArgumentNullException_ThrowsArgumentNullException()
+        {
+            var service = new LoanService();
+            Assert.Throws<ArgumentNullException>(() => service.Add(null));
+        }
+
+        [Theory]
+        [InlineData(null, "b1")]
+        [InlineData("r1", null)]
+        [InlineData("", "b1")]
+        [InlineData("r1", "")]
+        public void NullOrEmptyReaderIdOrBookId_ShouldThrowArgumentNullException_ThrowsArgumentNullException(string readerId, string bookId)
+        {
+            var service = new LoanService();
+            Assert.Throws<ArgumentNullException>(() => service.Get(readerId, bookId));
+        }
+
+        [Fact]
+        public void NotExistingLoan_ShouldThrowKeyNotFoundException_ThrowsKeyNotFoundException()
+        {
+            var service = new LoanService();
+            Assert.Throws<KeyNotFoundException>(() => service.Get("notexist", "missingbook"));
+        }
+    }
+}
--- NI_Project.Api/Services/LoanService.cs
+++ NI_Project.Api/Services/LoanService.cs
@@ -5,15 +5,40 @@ namespace NI_Project.Api.Services
 {
     public class LoanService : ILoanService
     {
-        private readonly List<Loan> _loans;
+        private readonly List<Loan> _loans = new List<Loan>();
         public void Add(Loan loan)
         {
+            if (loan == null)
+            {
+                throw new ArgumentNullException(nameof(loan));
+            }
+
+            if (_loans.Any(l => l.ReaderId == loan.ReaderId && l.BookId == loan.BookId))
+            {
+                throw new ArgumentException($"Loan already exists for reader '{loan.ReaderId}' and book '{loan.BookId}'.");
+            }                
+
             _loans.Add(loan);
         }
 
         public Loan Get(string readerId, string bookId)
         {
-            return _loans.Find(x => x.ReaderId == readerId && x.BookId == bookId);
+            if (string.IsNullOrWhiteSpace(readerId))
+            {
+                throw new ArgumentNullException(nameof(readerId));
+            }
+            if (string.IsNullOrWhiteSpace(bookId))
+            {
+                throw new ArgumentNullException(nameof(bookId));
+            }
+
+            var loan = _loans.Find(x => x.ReaderId == readerId && x.BookId == bookId);
+            if (loan == null)
+            {
+                throw new KeyNotFoundException($"Loan not found for reader '{readerId}' and book '{bookId}'.");
+            }
+
+            return loan;
         }
 
         public List<Loan> GetAll()
