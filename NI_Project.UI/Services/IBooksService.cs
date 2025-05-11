6284391ab0bc24cc8c67311fbc9ff9c1b48bba75
Kalax <erdelyi.peter04@gmail.com>
2025-05-11 10:35:12 +0200

    IBooksService.cs created and implemented.

---
+++ NI_Project.UI/Services/IBooksService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NI_Project.Shared;
+
+namespace NI_Project.UI.Services
+{
+    public interface IBooksService
+    {
+        Task<List<Book>> GetBooksAsync();
+
+        Task<Book> GetBookAsync(string id);
+
+        Task AddBookAsync(Book book);
+
+        Task UpdateBookAsync(Book book);
+
+        Task DeletePersonAsync(string id);
+    }
+}
