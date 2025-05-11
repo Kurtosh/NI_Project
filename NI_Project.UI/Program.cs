e9f0d374f60a1979d2f1032a5db4e44e51d182c3
Kalax <erdelyi.peter04@gmail.com>
2025-05-11 12:50:22 +0200

    Reader blazor components created and implemented.

--- NI_Project.UI/Program.cs
+++ NI_Project.UI/Program.cs
@@ -10,5 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7091") });
 
 builder.Services.AddScoped<IBooksService, BooksService>();
+builder.Services.AddScoped<IReadersService, ReadersService>();
 
 await builder.Build().RunAsync();
