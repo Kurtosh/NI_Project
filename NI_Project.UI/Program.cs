5c41313ebf27aaff8e909584415c6990d48fd09c
Kalax <erdelyi.peter04@gmail.com>
2025-05-11 11:56:37 +0200

    Home.razor changed, BookList.razor implemented.

--- NI_Project.UI/Program.cs
+++ NI_Project.UI/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using NI_Project.UI;
+using NI_Project.UI.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7091") });
+
+builder.Services.AddScoped<IBooksService, BooksService>();
 
 await builder.Build().RunAsync();
