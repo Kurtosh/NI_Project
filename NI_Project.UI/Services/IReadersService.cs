3b1e00f0175ac2fc864c59b8162e8e7520590dce
Kalax <erdelyi.peter04@gmail.com>
2025-05-11 12:22:22 +0200

    IReadersService.cs created and implemented.

---
+++ NI_Project.UI/Services/IReadersService.cs
@@ -0,0 +1,17 @@
+﻿using NI_Project.Shared;
+
+namespace NI_Project.UI.Services
+{
+    public interface IReadersService
+    {
+        Task<List<Reader>> GetReadersAsync();
+
+        Task<Reader> GetReaderAsync(string id);
+
+        Task AddReaderAsync(Reader reader);
+
+        Task UpdateReaderAsync(string id, Reader reader);
+
+        Task DeleteReaderAsync(string id);
+    }
+}
